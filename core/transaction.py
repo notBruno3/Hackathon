0bde5d1af522f1b01d5c5d16c62fd79f9ad20b79
Bruno Romero Perez <b.romero@student.tudelft.nl>
2025-05-03 03:41:17 +0200

    deleted duplicates

--- core/transaction.py
+++
@@ -1,9 +0,0 @@
-from typing import List
-
-class Transaction:
-    def __init__(self, id: str, payer: str, payees: List[str], amount: float, source_message_id: str):
-        self.id = id
-        self.payer = payer
-        self.payees = payees
-        self.amount = amount
-        self.source_message_id = source_message_id
